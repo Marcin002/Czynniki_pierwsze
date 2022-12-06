99cb0ec73b91dd28a0a76e8fcc38f3162c91f8ba
PC1 <marcinkummer99@gmail.com>
2022-12-06 17:43:29 +0100

    while(liczba>1)

--- app/src/main/java/com/example/czynniki_pierwsze/MainActivity.kt
+++ app/src/main/java/com/example/czynniki_pierwsze/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.czynniki_pierwsze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.wylicz).setOnClickListener {
+            var liczba=findViewById<EditText>(R.id.wprowadz).text.toString().toInt()
+            var czynnik=2
+            var wynik=" "
+
+            while (liczba>1){
+                while(liczba%czynnik==0){
+
+                    wynik+=liczba
+                    liczba=liczba/czynnik
+
+                }//liczba%czynnik==0
+                czynnik+=1
+
+            }//liczba>1
+
+        }
+
+
     }
 }
